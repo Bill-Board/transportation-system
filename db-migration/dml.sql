653ce3ca296290615c496a809405b571644d40fb
aswan-kib <237621142+aswan-kib@users.noreply.github.com>
2025-10-26 16:44:04 +0600

    refactor dml file

--- db-migration/dml.sql
+++ db-migration/dml.sql
@@ -2,7 +2,7 @@ INSERT INTO employee (id, created, updated, version, date_of_birth, driving_stat
 SELECT NEXTVAL('employee_id_sequence'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, '1999-03-20', 0, 'Akibul', 'MALE', 'Doe', '01815980623', 'ACTIVE';
 
 INSERT INTO auth_info (id, created, updated, version, email, hased_password, salt, employee_id)
-SELECT NEXTVAL('auth_id_sequence'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, 'admin@gmail.com', 'd5070b89996754bdb7a8c773a335b3a78ea3ae0a19f7cf54d933279ef26400fc', 'a118915d123da3c7e1e970ad47e18c8b', 1;
+SELECT NEXTVAL('auth_id_sequence'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, 'admin@gmail.com', 'e184d2c35b768f2898887e7c7b74d4c1a9298e90d3d013c9d30543b8ee19c29f', 'a118915d123da3c7e1e970ad47e18c8b', 1;
 
 INSERT INTO employee_types (id, employee_type)
 VALUES (1, 'SUPER_ADMIN'), (1, 'ADMIN');
